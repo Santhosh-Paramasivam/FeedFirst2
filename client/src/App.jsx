2abb6416a6ce5c0edef47f87fb24bfea1361510c
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2025-07-17 23:00:00 +0530

    New pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Login from './Common/Login'
 import Navbar from './Common/Navbar'
-import Register from './Recipient/Register'
+import Register from './Recipient/RegisterUser'
 import PantryDetails from './Common/PantryDetails'
 import Dashboard from './Recipient/Dashboard'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import RegisterUser from './Recipient/Register'
+import RegisterUser from './Recipient/RegisterUser'
 import Home from './Common/Home'
+import RegisterPantry from './PantryManager/RegisterPantry'
+import RegisterAdmin from './PantryManager/RegisterAdmin'
 
 function App() {
   return <BrowserRouter>
@@ -14,6 +16,8 @@ function App() {
       <Route path='/' element={<Home />}></Route>
       <Route path='/register_user' element={<RegisterUser />}></Route>
       <Route path='/pantries' element={<PantryDetails/>}></Route>
+      <Route path='/register_pantry' element={<RegisterPantry/>}></Route>
+      <Route path='/register_admin' element={<RegisterAdmin/>}></Route>
     </Routes>
   </BrowserRouter>
 }
